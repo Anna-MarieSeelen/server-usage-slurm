26102862c6ac0ae5dd15372f557422bec778ace2
Anna-MarieSeelen <annamarie.seelen@gmail.com>
2024-03-07 15:33:12 +0100

    first commit

---
+++ track_server_usage/__init__.py
@@ -0,0 +1,8 @@
+"""Documentation about track_server_usage."""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Anna-Marie Seelen"
+__email__ = "annamarie.seelen@gmail.com"
+__version__ = "0.1.0"
